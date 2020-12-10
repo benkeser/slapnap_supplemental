07ef5b8f2eae43b4f5f23ca97cf825e2c7ac34bc
Brian Williamson <brianw26@uw.edu>
2020-12-10 09:53:16 -0800

    put CIs on logit scale to match with slapnap

--- single_sensitivity/R/combine_results.R
+++ single_sensitivity/R/combine_results.R
@@ -17,33 +17,46 @@ ct <- 0
 for(folder_root in folder_roots){
 	ct <- ct + 1
 	print(folder_root)
-	orig_files <- list.files(here("bash_output", folder_root))
+	orig_files <- list.files(here("docker_output", folder_root))
 
 	# assumes only a single csv is present
 	orig_data_csv <- orig_files[grep(".csv", orig_files)]
-	orig_data <- read.csv(here("bash_output", folder_root, orig_data_csv))
+	orig_data <- read.csv(here("docker_output", folder_root, orig_data_csv))
 
 	# load learner
 	rds_files <- orig_files[grep(".rds", orig_files)]
 	sens_files <- rds_files[grep("sens", rds_files)]
-	# assumes two files, one starts with cv
-	sens_fit <- readRDS(here("bash_output", folder_root, sens_files[2]))
-	stopifnot(class(sens_fit) == "SuperLearner")
+	# assumes two files, first one starts with 'cv'
+	sens_fit <- readRDS(here("docker_output", folder_root, sens_files[1]))
+	stopifnot(class(sens_fit) == "CV.SuperLearner")
 
 	# predictions and evaluations
-	sens_pred <- predict(sens_fit)
-	sens_sl_pred <- as.numeric(sens_pred[[1]])
+	sens_sl_pred <- as.numeric(sens_fit$SL.predict)
 
 	# auc + ci
-	est_sens_auc <- ci.cvAUC(predictions = sens_sl_pred, labels = orig_data[, "sens"])
+    ests <- vector("list", length = sens_fit$V)
+    ses <- vector("list", length = sens_fit$V)
+    for (v in 1:sens_fit$V) {
+        est_sens_auc <- ci.cvAUC(predictions = sens_sl_pred[sens_fit$folds[[v]]], labels = sens_fit$Y[sens_fit$folds[[v]]])
+        ests[[v]] <- est_sens_auc$cvAUC
+        ses[[v]] <- est_sens_auc$se
+    }
+    est_vec <- unlist(ests)
+    se_vec <- unlist(ses)
+    mn_est <- mean(est_vec)
+    mn_se <- mean(se_vec)
+    grad <- 1 / (mn_est - mn_est ^ 2)
+    logit_se <- sqrt(mn_se ^ 2 * grad ^ 2)
+    cil <- plogis(qlogis(mn_est) - qnorm(0.975) * logit_se)
+    ciu <- plogis(qlogis(mn_est) + qnorm(0.975) * logit_se)
 
-	out[[ct]] <- est_sens_auc
+	out[[ct]] <- list(AUC = mn_est, ci = c(cil, ciu))
 }
 
 bnab <- AUC <- cil <- ciu <- NULL
 for(i in folder_roots){
 	bnab <- c(bnab, i)
-	AUC <- c(AUC, out[[i]]$cvAUC)
+	AUC <- c(AUC, out[[i]]$AUC)
 	cil <- c(cil, out[[i]]$ci[1])
 	ciu <- c(ciu, out[[i]]$ci[2])
 }
